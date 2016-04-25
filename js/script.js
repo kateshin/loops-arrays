2d833dbe8330c30a6e0f3421f5e1b385b41704f8
kateshin <kateshin@wustl.edu>
2016-04-25 18:04:19 -0500

    followed lab

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ var count = 0;
 var list = '';
 
 while (count < 10) {
-  list += '<li>current count is: ' + count + '</li>';
+  list += '<ol> current count is: ' + count + '</ol';
   count += 1;
 }
 
 // output results
 document.write("<h1>Loops</h1>");
-document.write("<ul>" + list + "</ul>");
+document.write("<ol>" + list + "</ol>");
+
+// array:
+
+var myItems = [
+  ["question one", "answer one"],
+  ["question two", "answer two"]
+];
+
+document.write("<p> " + myItems [1][1] + "</p>");
